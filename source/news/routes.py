4ab40ec587c19ca4eae78322407c5b42ba6f4da7
Adil Shamuratov <iswearican_1@outlook.com>
2023-07-09 16:42:30 +0600

    clear

--- source/news/routes.py
+++ source/news/routes.py
@@ -27,4 +27,4 @@ async def get_news_page(request: Request, language: str):
             'title': news_title_data,
             'data': news_data
         },
-        status_code=status.HTTP_200_OK)
+        status_code=status.HTTP_200_OK, headers={})
